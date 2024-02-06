c09213a0f8a52cc88a66029366d568bf29189500
erwin <erwinsetyoprasetyo@gmail.com>
2024-02-07 06:33:08 +0700

    Adding Notebook For Menu

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 from datetime import datetime
 import pandas as pd
 from tkinter import filedialog
+from datetime import datetime, timezone
 
 
 root = Tk()
@@ -14,17 +15,22 @@ root = Tk()
 conn = sqlite3.connect("pinjamanpuskesmas.db")
 cursor = conn.cursor()
 
+# Create a notebook
+notebook = ttk.Notebook(root)
+notebook.pack(fill="both", padx=20, pady=10)
+
 
 def create_table():
     # Menonaktifkan pengecekan kunci asing
     cursor.execute("PRAGMA foreign_keys=off")
+    cursor.execute("PRAGMA timezone = 'Asia/Jakarta'")
     cursor.execute("DROP TABLE IF EXISTS LOANS")
     query = """
     CREATE TABLE LOANS(
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         NAMA TEXT NOT NULL,
         NIP INTEGER NOT NULL,
-        INSTANSI TEXT NOT NULL,
+        PUSKESMAS TEXT NOT NULL,
         TANGGAL_LAHIR TEXT NOT NULL,
         ALAMAT TEXT NOT NULL,
         JUMLAH_PINJAMAN INTEGER NOT NULL,
@@ -33,11 +39,12 @@ def create_table():
         BAGI_HASIL INT,
         POKOK INT,
         TERIMA_BERSIH INT,
-        URAIAN TEXT
+        TIMESTAMP TIMESTAMP DEFAULT (datetime('now', 'localtime'))
     )
     """
     cursor.execute(query)
     cursor.execute("PRAGMA foreign_keys=on")
+    cursor.execute("PRAGMA timezone = DEFAULT")
     conn.commit()
 
 
@@ -61,7 +68,7 @@ def is_valid_date(date_str):
 
 
 def select_all():
-    query = "SELECT ID, NAMA, NIP, INSTANSI, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH, URAIAN FROM LOANS"
+    query = "SELECT ID, NAMA, NIP, PUSKESMAS, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH FROM LOANS"
     cursor.execute(query)
     rows = cursor.fetchall()
     update_trv(rows)
@@ -78,15 +85,14 @@ def add_new():
         # Ambil nilai dari setiap field
         nama_value = v_nama.get()
         nip_value = v_nip.get()
-        instansi_value = v_instansi.get()
+        puskesmas_value = v_puskesmas.get()
         tanggal_lahir_value = v_tanggal_lahir.get()
         alamat_value = v_alamat_rumah.get()
         jumlah_pinjaman_value = v_jumlah_pinjaman.get()
         jangka_waktu_value = v_jangka_waktu.get()
-        uraian_value = v_uraian.get()
         resiko_kredit_value = 1.5/100 * jumlah_pinjaman_value
         bagi_hasil_value = 0.01 * jumlah_pinjaman_value
-
+        pokok_value = jumlah_pinjaman_value / jangka_waktu_value
 
         if not nama_value or not nip_value or not tanggal_lahir_value or not jumlah_pinjaman_value or not jangka_waktu_value:
             messagebox.showerror("Error", "Semua field harus diisi.")
@@ -96,7 +102,7 @@ def add_new():
             messagebox.showerror(
                 "Error", "Jumlah pinjaman harus berupa bilangan bulat positif")
             return
-        
+
         if (jangka_waktu_value > 48):
             messagebox.showerror(
                 "Error", "Jangka Waktu tidak boleh dari 48 Bulan")
@@ -108,37 +114,41 @@ def add_new():
                 "Kesalahan", "Format tanggal lahir tidak valid")
             return
 
-        jumlah_pinjaman_int = int(jumlah_pinjaman_value)
+        confirm = messagebox.askquestion(
+            "Konfirmasi", "Apakah data sudah benar ?")
 
-        query = """
-            INSERT INTO LOANS
-            (NAMA, NIP, INSTANSI, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH, URAIAN)
-            values (:NAMA, :NIP, :INSTANSI, :TANGGAL_LAHIR, :ALAMAT, :JUMLAH_PINJAMAN, :JANGKA_WAKTU, :RESIKO_KREDIT, :BAGI_HASIL, :POKOK, :TERIMA_BERSIH, :URAIAN)
-            """
-        params = {
-            "NAMA": nama_value,
-            "NIP": nip_value,
-            "INSTANSI": instansi_value,
-            "ALAMAT": alamat_value,
-            "TANGGAL_LAHIR": tanggal_lahir_value,
-            "JUMLAH_PINJAMAN": jumlah_pinjaman_value,
-            "JANGKA_WAKTU": jangka_waktu_value,
-            "RESIKO_KREDIT": resiko_kredit_value,
-            "BAGI_HASIL": bagi_hasil_value,
-            "POKOK": jumlah_pinjaman_value / jangka_waktu_value,
-            "TERIMA_BERSIH": jumlah_pinjaman_value - resiko_kredit_value,
-            "URAIAN": uraian_value
-        }
-        cursor.execute(query, params)
-        conn.commit()
-        print("Data added Succesfully.")
-        select_all()
-        clear_field()
+        if confirm == 'yes':
+            # Jika user menekan Yes, Simpan data ke database
+            query = """
+                INSERT INTO LOANS
+                (NAMA, NIP, PUSKESMAS, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH, TIMESTAMP)
+                values (:NAMA, :NIP, :PUSKESMAS, :TANGGAL_LAHIR, :ALAMAT, :JUMLAH_PINJAMAN, :JANGKA_WAKTU, :RESIKO_KREDIT, :BAGI_HASIL, :POKOK, :TERIMA_BERSIH, CURRENT_TIMESTAMP)
+                """
+            params = {
+                "NAMA": nama_value,
+                "NIP": nip_value,
+                "PUSKESMAS": puskesmas_value,
+                "ALAMAT": alamat_value,
+                "TANGGAL_LAHIR": tanggal_lahir_value,
+                "JUMLAH_PINJAMAN": jumlah_pinjaman_value,
+                "JANGKA_WAKTU": jangka_waktu_value,
+                "RESIKO_KREDIT": resiko_kredit_value,
+                "BAGI_HASIL": bagi_hasil_value,
+                "POKOK": round(pokok_value / 100) * 100,
+                "TERIMA_BERSIH": jumlah_pinjaman_value - resiko_kredit_value
+            }
+            cursor.execute(query, params)
+            conn.commit()
+            messagebox.showinfo("Info", "Data Berhasil ditambahkan !")
+            print("Data added Succesfully.")
+            select_all()
+            clear_field()
+
     except Exception as e:
         print("Error :", e)
         messagebox.showerror(
             "Error", "Pastikan format data yang diupdate sudah benar")
-        
+
 
 def update_data():
     try:
@@ -157,7 +167,7 @@ def update_data():
                 messagebox.showerror(
                     "Error", "Jumlah pinjaman harus berupa bilangan bulat positif")
                 return
-            
+
             if (jangka_waktu_value > 48):
                 messagebox.showerror(
                     "Error", "Jangka Waktu tidak boleh dari 48 Bulan")
@@ -165,12 +175,12 @@ def update_data():
 
             query = """ 
                 UPDATE LOANS
-                SET NAMA=?, NIP=?, INSTANSI=?, ALAMAT=?, TANGGAL_LAHIR=?, JUMLAH_PINJAMAN=?, JANGKA_WAKTU=?, URAIAN=? WHERE ID=?
+                SET NAMA=?, NIP=?, PUSKESMAS=?, ALAMAT=?, TANGGAL_LAHIR=?, JUMLAH_PINJAMAN=?, JANGKA_WAKTU=?, URAIAN=? WHERE ID=?
             """
             params = (
                 v_nama.get(),
                 v_nip.get(),
-                v_instansi.get(),
+                v_puskesmas.get(),
                 v_alamat_rumah.get(),
                 v_tanggal_lahir.get(),
                 v_jumlah_pinjaman.get(),
@@ -192,20 +202,20 @@ def update_data():
         messagebox.showerror(
             "Error", "Pastikan format data yang diupdate sudah benar")
 
+
 def getrow(event):
     rowid = trv.identify_row(event.y)
     item = trv.item(rowid)
 
-    if 'values' in item and len(item['values']) >= 14:
+    if 'values' in item and len(item['values']) >= 13:
         v_id.set(item['values'][0])
         v_nama.set(item['values'][2])
         v_nip.set(item['values'][3])
-        v_instansi.set(item['values'][4])
+        v_puskesmas.set(item['values'][4])
         v_tanggal_lahir.set(item['values'][5])
         v_alamat_rumah.set(item['values'][6])
         v_jumlah_pinjaman.set(item['values'][7])
         v_jangka_waktu.set(item['values'][8])
-        v_uraian.set(item['values'][13])
     else:
         print("Error: Insufficient values in the 'values atribute.")
 
@@ -213,12 +223,11 @@ def getrow(event):
 def clear_field():
     entry_nama.delete(0, 'end')
     entry_nip.delete(0, 'end')
-    entry_instansi.delete(0, 'end')
+    entry_puskesmas.delete(0, 'end')
     dob_entry.delete(0, 'end')
     address_entry.delete(0, 'end')
     jumlahpinjaman_entry.delete(0, 'end')
     jangkawaktu_entry.delete(0, 'end')
-    uraian_entry.delete(0, 'end')
 
 
 def delete_data():
@@ -251,7 +260,7 @@ def export_data():
 
         # Membuat DataFrame dari data
         df = pd.DataFrame(rows, columns=[
-            'ID', 'NAMA', 'NIP', 'INSTANSI', 'TANGGAL_LAHIR', 'ALAMAT', 'JUMLAH_PINJAMAN', 'JANGKA_WAKTU', 'RESIKO_KREDIT', 'BAGI_HASIL', 'POKOK', 'TERIMA_BERSIH', 'URAIAN'
+            'ID', 'NAMA', 'NIP', 'PUSKESMAS', 'TANGGAL_LAHIR', 'ALAMAT', 'JUMLAH_PINJAMAN', 'JANGKA_WAKTU', 'RESIKO_KREDIT', 'BAGI_HASIL', 'POKOK', 'TERIMA_BERSIH', 'URAIAN'
         ])
 
         # Menyimpan DataFrame ke file Excel
@@ -268,7 +277,7 @@ def export_data():
 def search():
     q2 = q.get()
     query = """
-    SELECT ID, NAMA, NIP, INSTANSI, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH, URAIAN FROM LOANS WHERE NAMA LIKE {} OR NIP LIKE {}
+    SELECT ID, NAMA, NIP, PUSKESMAS, TANGGAL_LAHIR, ALAMAT, JUMLAH_PINJAMAN, JANGKA_WAKTU, RESIKO_KREDIT, BAGI_HASIL, POKOK, TERIMA_BERSIH FROM LOANS WHERE NAMA LIKE {} OR NIP LIKE {}
     """.format("'%"+q2+"%'", "'%"+q2+"%'")
     cursor.execute(query)
     conn.commit()
@@ -283,20 +292,38 @@ def clear():
 
 
 # Wrapper
-wrapper1 = LabelFrame(root, text="Daftar Peminjam")
-wrapper2 = LabelFrame(root, text="Pencarian")
-wrapper3 = LabelFrame(root, text="Data Peminjam")
+wrapperPencarian = LabelFrame(root, text="Pencarian")
+wrapperDataPeminjam = LabelFrame(root, text="Data Peminjam")
+
+# For tab (notebook)
+frame1 = ttk.Frame(notebook, width=400, height=280)
+frame2 = ttk.Frame(notebook, width=400, height=280)
+frame3 = ttk.Frame(notebook, width=400, height=280)
+frame4 = ttk.Frame(notebook, width=400, height=280)
+frame5 = ttk.Frame(notebook, width=400, height=280)
+
+frame1.pack(fill='both', expand=True)
+frame2.pack(fill='both', expand=True)
+frame3.pack(fill='both', expand=True)
+frame4.pack(fill='both', expand=True)
+frame5.pack(fill='both', expand=True)
+
+# Add Frames to notebook
+notebook.add(frame1, text='Semua')
+notebook.add(frame2, text='Per Jumlah Pinjaman')
+notebook.add(frame3, text='Per Resiko Kredit')
+notebook.add(frame4, text='Per Bagi Hasil')
+notebook.add(frame5, text='Per Sisa Pokok')
 
 # Posisi Wrapper
-wrapper1.pack(fill="both", expand="yes", padx=20, pady=10)
-wrapper2.pack(fill="both", padx=20, pady=10)
-wrapper3.pack(fill="both", padx=20, pady=10)
+wrapperPencarian.pack(fill="both", padx=20, pady=10)
+wrapperDataPeminjam.pack(fill="both", padx=20, pady=10)
 
 # Form variable
 v_id = IntVar()
 v_nama = StringVar()
 v_nip = IntVar()
-v_instansi = StringVar()
+v_puskesmas = StringVar()
 v_tanggal_lahir = StringVar()
 v_alamat_rumah = StringVar()
 v_jumlah_pinjaman = IntVar()
@@ -304,7 +331,7 @@ v_jangka_waktu = IntVar()
 v_uraian = StringVar()
 
 # Frame untuk Form di Wrapper 3
-form_frame = ttk.Frame(wrapper3, padding="5")
+form_frame = ttk.Frame(wrapperDataPeminjam, padding="5")
 form_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
 # Label dan Entry untuk Nama
@@ -319,11 +346,11 @@ label_nip.grid(row=1, column=0, sticky="w", padx=5, pady=5)
 entry_nip = ttk.Entry(form_frame, textvariable=v_nip)
 entry_nip.grid(row=1, column=1, padx=5, pady=5)
 
-# Label dan Entry untuk Instansi
-label_instansi = ttk.Label(form_frame, text="Instansi")
-label_instansi.grid(row=2, column=0, sticky="w", padx=5, pady=5)
-entry_instansi = ttk.Entry(form_frame, textvariable=v_instansi)
-entry_instansi.grid(row=2, column=1, padx=5, pady=5)
+# Label dan Entry untuk Puskesmas
+label_puskesmas = ttk.Label(form_frame, text="Puskesmas")
+label_puskesmas.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+entry_puskesmas = ttk.Entry(form_frame, textvariable=v_puskesmas)
+entry_puskesmas.grid(row=2, column=1, padx=5, pady=5)
 
 # Label dan Entry untuk alamat Rumah
 label_address = ttk.Label(form_frame, text="Alamat Rumah")
@@ -352,14 +379,9 @@ label_jangkawaktu.grid(row=2, column=2, sticky="w", padx=5, pady=5)
 jangkawaktu_entry = ttk.Entry(form_frame, textvariable=v_jangka_waktu)
 jangkawaktu_entry.grid(row=2, column=3, sticky="w", padx=5, pady=5)
 
-# Label dan Entry untuk Uraian
-label_uraian = ttk.Label(form_frame, text="Uraian")
-label_uraian.grid(row=3, column=2, sticky="w", padx=5, pady=5)
-uraian_entry = ttk.Entry(form_frame, textvariable=v_uraian)
-uraian_entry.grid(row=3, column=3, sticky="w", padx=5, pady=5)
 
 # Frame button add, update, delete
-frame_btn = Frame(wrapper3)
+frame_btn = Frame(wrapperDataPeminjam)
 update_btn = Button(frame_btn, text="Update", command=update_data)
 add_btn = Button(frame_btn, text="Tambah Data", command=add_new)
 delete_btn = Button(frame_btn, text="Hapus", command=delete_data)
@@ -373,18 +395,18 @@ export_btn.pack(side=LEFT, padx=5)
 
 # Wrapper 2 - Pencarian
 q = StringVar()
-lbl = Label(wrapper2, text="Search")
+lbl = Label(wrapperPencarian, text="Search")
 lbl.pack(side=LEFT, padx=10, pady=15)
-ent = Entry(wrapper2, textvariable=q)
+ent = Entry(wrapperPencarian, textvariable=q)
 ent.pack(side=LEFT, padx=6, pady=15)
-btn = Button(wrapper2, text="Search", command=search)
+btn = Button(wrapperPencarian, text="Search", command=search)
 btn.pack(side=LEFT, padx=6, pady=15)
-cbtn = Button(wrapper2, text="Clear", command=clear)
+cbtn = Button(wrapperPencarian, text="Clear", command=clear)
 cbtn.pack(side=LEFT, padx=6)
 
-# Wrapper 1 - Tabel dari data simpan pinjam
-trv = ttk.Treeview(wrapper1, column=(0, 1, 2, 3, 4, 5, 6, 7,
-                   8, 9, 10, 11, 12, 13), show="headings", height=12)
+# Frame1 - Tampilkan semua dari database
+trv = ttk.Treeview(frame1, column=(0, 1, 2, 3, 4, 5, 6, 7,
+                   8, 9, 10, 11, 12), show="headings", height=12)
 style = ttk.Style()
 # ["aqua", "step", "clam", "alt", "default", "classic"]
 style.theme_use("clam")
@@ -395,7 +417,7 @@ trv.heading(0, text="Id")
 trv.heading(1, text="No")
 trv.heading(2, text="Nama")
 trv.heading(3, text="NIP")
-trv.heading(4, text="Instansi")
+trv.heading(4, text="Puskesmas")
 trv.heading(5, text="Tanggal Lahir")
 trv.heading(6, text="Alamat Rumah")
 trv.heading(7, text="Jumlah Pinjaman")
@@ -404,7 +426,6 @@ trv.heading(9, text="Resiko Kredit")
 trv.heading(10, text="Bagi Hasil")
 trv.heading(11, text="Pokok")
 trv.heading(12, text="Terima Bersih")
-trv.heading(13, text="Uraian")
 
 trv.column(0, stretch=NO, width=0)
 trv.column(1, width=5, minwidth=70, anchor=CENTER)
@@ -419,22 +440,57 @@ trv.column(9, width=85, minwidth=120, anchor=CENTER)
 trv.column(10, width=85, minwidth=120, anchor=CENTER)
 trv.column(11, width=60, minwidth=120, anchor=CENTER)
 trv.column(12, width=80, minwidth=120, anchor=CENTER)
-trv.column(13, width=86, minwidth=120, anchor=CENTER)
+
+
+# Frame 2 - Tampilkan per Jumlah Pinjaman
+trv2 = ttk.Treeview(frame2, column=(0, 1, 2, 3, 4, 5),
+                    show="headings", height=12)
+style = ttk.Style()
+
+style.theme_use("clam")
+trv2.pack(side=RIGHT)
+trv2.place(x=0, y=0)
+
+trv2.heading(0, text="Id")
+trv2.heading(1, text="No")
+trv2.heading(2, text="Nama")
+trv2.heading(3, text="Puskesmas")
+trv2.heading(4, text="Tanggal Realisasi")
+trv2.heading(5, text="Jumlah Pinjaman")
+
+trv2.column(0, stretch=NO, width=0)
+trv2.column(1, width=5, minwidth=70, anchor=CENTER)
+trv2.column(2, width=80 ,minwidth=120, anchor=CENTER)
+trv2.column(3, width=80 ,minwidth=120, anchor=CENTER)
+trv2.column(4, width=80 ,minwidth=120, anchor=CENTER)
+trv2.column(5, width=80 ,minwidth=120, anchor=CENTER)
+trv2.column(5, width=120 ,minwidth=120, anchor=CENTER)
+
 
 # fungsi untuk user klik 2x di trv
 trv.bind('<Double 1>', getrow)
 
-# Membuat scroll bar untuk tabel
-yscrollbar = Scrollbar(wrapper1, orient="vertical", command=trv.yview)
+# Membuat scroll bar untuk semua tabel
+yscrollbar = Scrollbar(frame1, orient="vertical", command=trv.yview)
 yscrollbar.pack(side=RIGHT, fill="y")
 
-xscrollbar = Scrollbar(wrapper1, orient="horizontal", command=trv.xview)
+xscrollbar = Scrollbar(frame1, orient="horizontal", command=trv.xview)
 xscrollbar.pack(side=BOTTOM, fill="x")
 
 trv.configure(yscrollcommand=yscrollbar.set, xscrollcommand=xscrollbar.set)
 
 # Always show the horizontal scrollbar
 trv.columnconfigure(0, weight=1)
+
+# Membuat scroll bar untuk Per Jumlah Pinjaman
+yscrollbar2 = Scrollbar(frame2, orient="vertical", command=trv2.yview)
+yscrollbar2.pack(side=RIGHT, fill="y")
+
+xscrollbar2 = Scrollbar(frame2, orient="horizontal", command=trv2.xview)
+xscrollbar2.pack(side=BOTTOM, fill="x")
+
+trv2.configure(yscrollcommand=yscrollbar2.set, xscrollcommand=xscrollbar2.set)
+
 
 
 if __name__ == '__main__':
